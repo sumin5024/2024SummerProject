590aacbd8d5a114fe616b67ad41c71a8aa39537f
EastOst <deltak2022@gmail.com>
2024-07-17 02:33:14 +0900

    어그로 아이템 완성
    이미지 추가하고 플레이어가 있던 곳으로 몰리게 수정

--- Assets/Scripts/Attract_Item.cs
+++ Assets/Scripts/Attract_Item.cs
@@ -4,39 +4,72 @@ using UnityEngine;
 
 public class Attract_Item : MonoBehaviour
 {
+    public Transform playerTransform; 
+    private Vector3 targetPosition;
     public float attractDuration = 10f;
     public float cooldownDuration = 20f;
     private bool isAttractActive = false;
     private bool isCooldown = false;
-    private Vector3 targetPosition;
+
+    public GameObject attractItemImage; 
+
+    void Start()
+    {
+        if (attractItemImage != null)
+        {
+            attractItemImage.SetActive(false); 
+        }
+    }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.P) && !isCooldown)
         {
+            Debug.Log("P key pressed, storing player position");
+            StorePlayerPosition();
             UseAttractItem();
         }
+    }
 
-        if (isAttractActive)
+    void StorePlayerPosition()
+    {
+        if (playerTransform != null)
         {
-            targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            targetPosition.z = 0;
+            targetPosition = playerTransform.position;
+            targetPosition.z = 0; 
+            Debug.Log("Target position set to: " + targetPosition);
+        }
+        else
+        {
+            Debug.LogError("Player Transform is not assigned.");
         }
     }
 
     void UseAttractItem()
     {
+        if (attractItemImage != null)
+        {
+            attractItemImage.transform.position = targetPosition; 
+            attractItemImage.SetActive(true); 
+        }
         StartCoroutine(ActivateAttract());
     }
 
     IEnumerator ActivateAttract()
     {
+        Debug.Log("Attract item activated");
         isAttractActive = true;
         isCooldown = true;
         yield return new WaitForSeconds(attractDuration);
         isAttractActive = false;
+        if (attractItemImage != null)
+        {
+            attractItemImage.SetActive(false); 
+        }
+        Debug.Log("Attract item deactivated");
         yield return new WaitForSeconds(cooldownDuration);
         isCooldown = false;
+        Debug.Log("Attract item cooldown finished");
     }
 
     public bool IsAttractActive()
